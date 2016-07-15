4e511c91646c2bcab8fb6ef2c9859ca3be07ff02
Albert Gao <Albert Gao>
2016-07-15 14:30:21 +1200

    1st commit

---
+++ lab1/print_prime.c
@@ -0,0 +1,27 @@
+//
+// Created by camus on 15/07/2016.
+//
+#include <stdio.h>
+
+int is_prime(int candidate) {
+    int i, result = 1;
+    for (i = 2; i <= candidate-1; i++) {
+        if (candidate % i == 0) {
+            result = 0;
+            break;
+        }
+    }
+    return result;
+}
+
+int main() {
+    int candidate = 2, num_printed = 0;
+    while (num_printed < 100) {
+        if (is_prime(candidate) == 1) {
+            printf("%d\n", candidate);
+            num_printed+=1;
+        }
+        candidate+=1;
+    }
+    return 0;
+}
