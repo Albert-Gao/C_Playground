136f43086fa643f9b9e8207190bec001faea433f
Albert Gao <albertgaohy@Gmail.com>
2016-07-22 15:47:26 +1200

    optimize the lab2

--- lab2/readSkating.c
+++ lab2/readSkating.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void insertion_sort(int arr[]) {
+void insertion_sort(double arr[]) {
     int start, compare, temp;
 
     for (start = 1; start < 3; start++) {
@@ -20,13 +20,20 @@ void insertion_sort(int arr[]) {
 void readOneline(int *winnerID, double *winnerScore){
     int nowID;
     double p1, p2, p3;
-    int arr[3];
+    double arr[3];
 
-    scanf("%d%lg%lg%lg\n",&nowID, &p1, &p2, &p3);
-    arr[0]=p1;
-    arr[1]=p2;
-    arr[2]=p3;
+    while (4==scanf("%d%lg%lg%lg\n",&nowID, &p1, &p2, &p3)){
+        arr[0]=p1;
+        arr[1]=p2;
+        arr[2]=p3;       
+    }
     insertion_sort(arr);
+
+    /*
+    printf("%f\n",arr[0]);
+    printf("%f\n",arr[1]);
+    printf("%f\n",arr[2]);
+    */
 
     if (arr[2] > *winnerScore){
         *winnerID = nowID;
@@ -35,11 +42,13 @@ void readOneline(int *winnerID, double *winnerScore){
 }
 
 int main(){
+    int i;
     int winnerID;
     double winnerScore=0;
-    readOneline(&winnerID, &winnerScore);
-    readOneline(&winnerID, &winnerScore);
-    readOneline(&winnerID, &winnerScore);
+
+    for (i=0; i<5; i++){
+        readOneline(&winnerID, &winnerScore);    
+    }
 
     printf("winner ID is %d\n", winnerID);
     printf("winner Score is %f\n", winnerScore);
