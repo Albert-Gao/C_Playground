360f966c1024c43e26f3657873f54205c8cf1d32
Albert Gao <albertgaohy@gmail.com>
2016-09-19 16:25:15 +1200

    lab20e selection sort done

---
+++ lab20/lab20e/flexarray.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "flexarray.h"
+
+struct flexarrayrec {
+    int capacity;
+    int itemcount;
+    int *items;
+};
+
+void *emalloc(size_t s) {
+    void *result = malloc(s);
+    if (NULL == result) {
+        fprintf(stderr, "Memory allocation failed!\n");
+        exit(EXIT_FAILURE);
+    }
+    return result;
+}
+
+void *erealloc(void *p, size_t s) {
+    void *result = realloc(p, s);
+    if (NULL == result) {
+        fprintf(stderr, "Memory re-allocation failed!\n");
+        exit(EXIT_FAILURE);
+    }
+    return result;
+}
+
+void selection_sort(int *arr, int size) {
+    int i, j, min_index, tempSwap, k;
+
+    for (i = 0; i < size - 1; i++) {
+        min_index = i;
+
+        for (j = i + 1; j < size; j++) {
+            if (arr[j] < arr[min_index]) {
+                min_index = j;
+            }
+        }
+
+        tempSwap = arr[min_index];
+        arr[min_index] = arr[i];
+        arr[i] = tempSwap;
+
+        if (i == ((size - 1) / 2)) {
+            for (k = 0; k < size; k++) {
+                fprintf(stderr, "%d\n", arr[k]);
+            }
+        }
+    }
+}
+
+flexarray flexarray_new() {
+    flexarray result = emalloc(sizeof *result);
+    result->capacity = 2;
+    result->itemcount = 0;
+    result->items = emalloc(result->capacity * sizeof result->items[0]);
+    return result;
+}
+
+void flexarray_append(flexarray f, int num) {
+    if (f->itemcount == f->capacity) {
+        f->capacity += f->capacity;
+        f->items = erealloc(f->items, f->capacity * sizeof f->items[0]);
+    }
+    f->items[f->itemcount++] = num;
+}
+
+void flexarray_print(flexarray f) {
+    int n = f->itemcount;
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d\n", f->items[i]);
+    }
+}
+
+void flexarray_sort(flexarray f) { selection_sort(f->items, f->itemcount); }
+
+void flexarray_free(flexarray f) {
+    free(f->items);
+    free(f);
+}
