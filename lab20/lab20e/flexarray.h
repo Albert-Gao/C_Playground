ac05cb9cccd704a103c22802b43ae50942b066c3
Albert Gao <albertgaohy@gmail.com>
2016-09-24 18:24:25 +1200

    delete no used code

--- lab20/lab20e/flexarray.h
+++ lab20/lab20e/flexarray.h
@@ -1,12 +1,8 @@
 #ifndef FLEXARRAY_H_
 #define FLEXARRAY_H_
-#include <stddef.h>
 
 typedef struct flexarrayrec *flexarray;
-/*
-extern void* emalloc(size_t);
-extern void* erealloc(void *, size_t);
-*/
+
 extern void flexarray_append(flexarray f, int item);
 extern void flexarray_free(flexarray f);
 extern flexarray flexarray_new();
