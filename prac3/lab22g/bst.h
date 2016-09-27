4037c69c9ad0fed44e908046435ff83ce84af071
DigitalAngle <albertgaohy@gmail.com>
2016-09-27 23:30:41 +1300

    add the practice for prac test 3

---
+++ prac3/lab22g/bst.h
@@ -0,0 +1,18 @@
+#ifndef BST_H_
+#define BST_H_
+
+/*DON'T NEED TO CODE THIS*/
+
+typedef struct bst_node *bst;
+/*
+bst b = bst_new();
+struct bstnode *b = bst_new();
+*/
+extern bst bst_free(bst b);
+extern void bst_inorder(bst b, void f(char *str));
+extern bst bst_insert(bst b, char *str);
+extern bst bst_new();
+extern void bst_preorder(bst b, void f(char *str));
+extern int bst_search(bst b, char *str);
+
+#endif
