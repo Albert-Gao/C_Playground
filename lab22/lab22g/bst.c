3fc2a3e7116510910c3574e53010dbf1818b77b0
DigitalAngle <albertgaohy@gmail.com>
2016-09-29 17:58:55 +1300

    fix original

--- lab22/lab22g/bst.c
+++ lab22/lab22g/bst.c
@@ -12,48 +12,49 @@ struct bstnode {
 
 bst bst_free(bst b) {
     free(b->key);
-    if (b->left != NULL)
-        bst_free(b->left);
-    if (b->right != NULL)
-        bst_free(b->right);
+    if (b->left != NULL) {
+        b->left=bst_free(b->left);
+    }
+    if (b->right != NULL) {
+        b->right=bst_free(b->right);
+    }
     free(b);
     return b;
 }
 
 bst bst_insert(bst b, char *str) {
-    /* create a new tree */
     if (b == NULL) {
         b = emalloc(sizeof(struct bstnode));
         b->left = NULL;
         b->right = NULL;
-        b->key = emalloc((strlen(str) + 1) * sizeof str[0]);
-        strcpy(b->key, str);
-    } else if (strcmp(str, b->key) > 0) {
-        /*the new value is greater than the old*/
-        b->right = bst_insert(b->right, str);
+        b->key = emalloc((strlen(str) + 1) * sizeof(b->key[0]));
+        strcpy(b->key, str);       
     } else if (strcmp(str, b->key) < 0) {
-        /*the new value is less then the old*/
         b->left = bst_insert(b->left, str);
+    } else if(strcmp(str,b->key)>0) {
+        b->right = bst_insert(b->right, str);
     }
     return b;
 }
 
-bst bst_new() { 
-    bst b=NULL;
-    return b; 
+bst bst_new() {
+    bst b = NULL;
+    return b;
 }
 
 void bst_preorder(bst b, void f(char *str)) {
-    if (b == NULL)
+    if (b == NULL) {
         return;
+    }
     f(b->key);
     bst_preorder(b->left, f);
     bst_preorder(b->right, f);
 }
 
 void bst_inorder(bst b, void f(char *str)) {
-    if (b == NULL)
+    if (b == NULL) {
         return;
+    }
     bst_inorder(b->left, f);
     f(b->key);
     bst_inorder(b->right, f);
@@ -62,11 +63,11 @@ void bst_inorder(bst b, void f(char *str)) {
 int bst_search(bst b, char *str) {
     if (b == NULL) {
         return 0;
-    } else if (strcmp(str, b->key) == 0) {
+    } else if (strcmp(b->key, str) == 0) {
         return 1;
-    } else if (strcmp(str, b->key) > 0) {
-        return bst_search(b->right, str);
-    } else {
+    } else if (strcmp(str, b->key) < 0) {
         return bst_search(b->left, str);
+    } else {
+        return bst_search(b->right, str);
     }
 }
