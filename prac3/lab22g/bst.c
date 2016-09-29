1ed37b24de90511361b987bdc7969721de2f453c
DigitalAngle <albertgaohy@gmail.com>
2016-09-29 17:24:34 +1300

    try new

--- prac3/lab22g/bst.c
+++ prac3/lab22g/bst.c
@@ -1,10 +1,8 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include "bst.h"
+#include <stdio.h>
 #include "mylib.h"
-
-/*NEED TO THIS WHOLE FILE!*/
+#include "bst.h"
+#include <string.h>
 
 struct bstnode {
     char *key;
@@ -12,56 +10,75 @@ struct bstnode {
     bst right;
 };
 
-bst bst_free(bst b) {
-    free(b->key);
-    if (b->left!=NULL) bst_free(b->left);
-    if(b->right!=NULL) bst_free(b->right);
-    free(b);
+bst bst_new() {
+    bst b = NULL;
     return b;
 }
 
 bst bst_insert(bst b, char *str) {
-    if (b==NULL){
-        bst b = emalloc(sizeof *b);
+    if(b == NULL) {
+        b = emalloc(sizeof(struct bstnode));
+        b->key = emalloc((strlen(str) + 1) * sizeof(str[0]));
+        strcpy(b->key, str);
         b->left = NULL;
         b->right = NULL;
-        b->key = emalloc((strlen(str)+1)*sizeof b->key[0]);
-        strcpy(b->key,str);
-    } else if (strcmp(str,b->key)>0){
-        b->right = bst_insert(b->right,str);
-    } else if (strcmp(str,b->key)<0){
-        b->left = bst_insert(b->left,str);
+    } else if (strcmp(str, b->key) < 0) {
+        b->left = bst_insert(b->left, str);
+    } else {
+        b->right = bst_insert(b->right, str);
     }
+
     return b;
 }
 
-bst bst_new() {
-    return NULL;
-}
-
-void bst_preorder(bst b, void f(char *str)) {
-    if (b==NULL) return;
-    f(b->key);
-    bst_preorder(b->left,f);
-    bst_preorder(b->right,f);
+int bst_search(bst b, char *str) {
+    if(b == NULL) {
+        return 0;
+    } else if (strcmp(b->key, str) == 0) {
+        return 1;
+    } else if (strcmp(str, b->key) < 0) {
+        return bst_search(b->left, str);
+    } else {
+        return bst_search(b->right, str);
+    }
 }
 
 void bst_inorder(bst b, void f(char *str)) {
-    if (b==NULL) return;
-    bst_inorder(b->left,f);
+    if(b == NULL) {
+        return;
+    }
+    if(b->left != NULL) {
+        bst_inorder(b->left, f);
+    }
     f(b->key);
-    bst_inorder(b->right,f);
+    if(b->right != NULL) {
+        bst_inorder(b->right, f);
+    }
 }
 
-int bst_search(bst b, char *str) {
-    if (b==NULL){
-        return 0;
-    } else if (strcmp(str,b->key)==0){
-        return 1;
-    } else if (strcmp(str,b->key)>0){
-        bst_search(b->right,str);
-    } else if (strcmp(str,b->key)<0){
-        bst_search(b->left,str);
-    } 
-    return 0;
+void bst_preorder(bst b, void f(char *str)) {
+    if(b == NULL) {
+        return;
+    }
+    f(b->key);
+    if(b->left != NULL) {
+        bst_preorder(b->left, f);
+    }
+    if(b->right != NULL) {
+        bst_preorder(b->right, f);
+    }
+}
+
+bst bst_free(bst b) {
+   free(b->key);
+
+   if(b->left != NULL) {
+     b->left = bst_free(b->left);
+   }
+
+   if(b->right != NULL) {
+    b->right = bst_free(b->right);
+   }
+   free(b);
+    return b;
 }
