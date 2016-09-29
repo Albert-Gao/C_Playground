f62fbf19d83c8a20bc50f55c6bfdfcd656bc2716
DigitalAngle <albertgaohy@gmail.com>
2016-09-30 11:19:21 +1300

    test

--- prac3/lab22i/queue-llist.c
+++ prac3/lab22i/queue-llist.c
@@ -6,15 +6,20 @@
 typedef struct q_item *q_item;
 
 struct q_item {
-
+    double item;
+    q_item next;
 };
 
 struct queue {
-
+    q_item first,last;
+    int length;
 };
 
 queue queue_new() {
-
+    queue q = emalloc(sizeof *q);
+    q->first = q->last = NULL;
+    q->length = 0;
+    return q;
 }
 
 void enqueue(queue q, double item) {
@@ -32,11 +37,27 @@ void enqueue(queue q, double item) {
 }
 
 double dequeue(queue q) {
+    if(q->length>0){
+        q_item node = q->first;
+        double pop = node->item;
 
+        q->first = node->next;
+        free(node);
+        q->length--;
+        return pop;
+    }
+    return -8.88;
 }
 
 void queue_print(queue q) {
-
+    q_item node;
+    if (q->length>0){
+        node = q->first;
+        while(node!=NULL){
+            printf("%.2f\n",node->item);
+            node=node->next;
+        }
+    }
 }
 
 void queue_print_info(queue q) {
@@ -50,9 +71,16 @@ void queue_print_info(queue q) {
 }
 
 int queue_size(queue q) { 
-
+    return q->length;
 }
 
 queue queue_free(queue q) {
-
+    q_item node;
+    while(q->first!=NULL){
+        node = q->first;
+        q->first = node->next;
+        free(node);
+    }
+    free(q);
+    return q;
 }
