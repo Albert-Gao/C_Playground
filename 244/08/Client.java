b5fad68cb7a736f9eb40de262073742e394f6b47
DigitalAngle <albertgaohy@gmail.com>
2016-10-05 21:02:18 +1300

    optimize the code style

--- 244/08/Client.java
+++ 244/08/Client.java
@@ -6,14 +6,13 @@ public class Client {
     public static void main(String[] args){
         Socket socket = null;
         try{
-            int port = Integer.parseInt(args[0]);
-            socket = new Socket(args[1], port);
+            socket = new Socket(args[1], Integer.parseInt(args[0]));
             System.err.println("Connect to the server");
+            
             new ReadWriteThread(System.in, socket.getOutputStream()).start();
             new ReadWriteThread(socket.getInputStream(), System.out).start();
         } catch (Exception e){
             e.printStackTrace();
         }
     }
-
 }
