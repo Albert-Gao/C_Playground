a806beb6ff3512004d58203e56767ba7ba26cd1c
albertgao <albertgaohy@gmail.com>
2016-10-05 15:46:14 +1300

    add the 244 part of code in order to prepare for the test

---
+++ 244/08/Client.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+    public static void main(String[] args){
+        Socket socket = null;
+        try{
+            int port = Integer.parseInt(args[0]);
+            socket = new Socket(args[1], port);
+            System.err.println("Connect to the server");
+            new ReadWriteThread(System.in, socket.getOutputStream()).start();
+            new ReadWriteThread(socket.getInputStream(), System.out).start();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
